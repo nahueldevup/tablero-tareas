d78b9756a599120fcda65db45f1fe02ba00d3e5a
nahuel <cuenta3fne@gmail.com>
2025-11-25 05:29:27 -0300

    fix:arreglos menores

--- app/Events/TaskUpdated.php
+++ app/Events/TaskUpdated.php
@@ -7,20 +7,20 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Queue\SerializesModels;
 
-class TaskUpdated implements ShouldBroadcastNow
+class TaskUpdated implements ShouldBroadcastNow // En cuanto ocurra este evento, transmítelo inmediatamente, no lo pongas en una cola de espera larga
 {
     use SerializesModels;
 
-    public function __construct(public Task $task, public string $action) {}
+    public function __construct(public Task $task, public string $action) {}// Recibe la tarea que cambió y qué pasó con ella ('created', 'updated', 'deleted')
 
     public function broadcastOn(): Channel
     {
-        return new Channel('tasks');
+        return new Channel('tasks'); // Define el "canal de radio" o "sala de chat" donde se emitirá el mensaje.
     }
 
     public function broadcastAs(): string
     {
-        return 'task.updated';
+        return 'task.updated'; // Define el nombre del evento. Por defecto, Laravel usa el nombre completo de la clase (App\Events\TaskUpdated), pero aquí lo estás personalizando para que sea más corto y limpio.
     }
 
     public function broadcastWith(): array
@@ -32,7 +32,7 @@ class TaskUpdated implements ShouldBroadcastNow
                 'title' => $this->task->title,
                 'user' => $this->task->user,
                 'status' => $this->task->status,
-            ]
+            ],
         ];
     }
 }
